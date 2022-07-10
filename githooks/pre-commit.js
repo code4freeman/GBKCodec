82856e06947f77403a6ba01195883a5fae3a7cf7
lilindog <lilin@lilin.site>
2022-07-10 14:39:21 +0800

    修改构建相关脚本

--- githooks/pre-commit.js
+++ githooks/pre-commit.js
@@ -1,14 +1,13 @@
 "use strict"
 
-const { runCmds } = require("./comm");
-const chalk = require("chalk");
+const { runCmds } = require("../scripts/comm");
 
 !async function () {
     if (!await runCmds(["npm run lint"])) {
-        console.log(chalk.red("\n): 代码 eslint 检测不通过, 请检查规范！\n"));
+        console.log("\n): 代码 eslint 检测不通过, 请检查规范！\n");
         process.exit(1);
     } else {
-        console.log(chalk.green("\n(: eslint 代码检测通过\n"));
+        console.log("\n(: eslint 代码检测通过\n");
     }
     // if (!await runCmds(["npm test"])) {
     //     console.log(chalk.red("\n): 单元测试不通过，请重新检查！"));
--- scripts/init-githooks.js
+++ scripts/init-githooks.js
@@ -1,20 +1,13 @@
 "use strict"
 
-/**
- * 初始化githook 
- * 
- * 需要脚本目录有相应的文件才可以被正确部署。
- * 脚本问文件名字需要 githook_ 作为前缀，后面的明明参考git文档给出的hook文件名，需要有js扩展名。
- */
-
 const { writeFileSync, existsSync, readdirSync } =require("fs");
-const { parse } = require("path");
+const { resolve, sep } = require("path");
 const platform = require("os").platform();
 const { execSync } = require("child_process");
 
-// 存放脚本的目录
 const 
-    SCRIPTS_DIR = "scripts",
+    HOOKS_SCRIPT_DIR = resolve(__dirname, "../githooks"),
+    HOOKS_DIR = resolve(__dirname, "../.git/hooks"),
     IS_THIS_PROJECT = (process.env.INIT_CWD || "").endsWith(process.env.npm_package_name);
 
 IS_THIS_PROJECT && main();
@@ -22,15 +15,15 @@ IS_THIS_PROJECT && main();
 function main () {
     console.log("开始部署本地githooks >>");
     const writedScripts = [], reg = /pre-\w+(?=\.js)/;
-    readdirSync("scripts").filter(i => reg.test(i)).map(i => i.match(reg)).forEach(name => {
-        const path = `${SCRIPTS_DIR}/${name}.js`;
+    readdirSync(HOOKS_SCRIPT_DIR).filter(i => reg.test(i)).map(i => i.match(reg)).forEach(name => {
+        const path = resolve(HOOKS_SCRIPT_DIR, name + ".js");
         if (existsSync(path)) {
             writedScripts.push(path);
-            writeHookFile(path);
+            writeHookFile(name);
         }
     });
     if (writedScripts.length) {
-        platform !== "win32" && execSync(`chmod -R 777 .git/hooks`);
+        platform !== "win32" && execSync(`chmod -R 777 ${resolve(HOOKS_DIR)}`);
         console.log(`githooks 部署完成，共有${writedScripts.length}个hook被部署：`);
         writedScripts.forEach((path, index) => {
             console.log(`(${index + 1}) -> ${path}`);
@@ -41,12 +34,14 @@ function main () {
     console.log("");
 }
 
-function writeHookFile (path) {
+function writeHookFile (name) {
+    const path = resolve(HOOKS_DIR, String(name));
+    const scriptPath = resolve(HOOKS_SCRIPT_DIR, name + ".js");
     writeFileSync(
-        `.git/hooks/${parse(path).name}`, 
+        path,
         `#!/usr/bin/env node
         const { spawn } = require("child_process");
-        const cp = spawn("node", ["${path}"], { stdio: "inherit" });
+        const cp = spawn("node", ["${scriptPath.replaceAll("\\", "\\\\")}"], { stdio: "inherit" });
         cp.on("exit", code => {
             process.exit(code);
         });
